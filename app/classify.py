692725d00ccfb478c920dee9ca5376906fac187e
Dade Sheng <dade@live.cn>
2017-02-08 18:41:00 -0500

    test prediction

---
+++ app/classify.py
@@ -0,0 +1,18 @@
+from deep_models.resnet50 import ResNet50
+# from deep_models.vgg19 import VGG19
+# from deep_models.vgg16 import VGG16
+from keras.preprocessing import image
+from deep_models.imagenet_utils import preprocess_input, decode_predictions
+import numpy as np
+
+
+def classify_image(img_path):
+    model = ResNet50(weights='imagenet')
+
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+
+    preds = model.predict(x)
+    return decode_predictions(preds)
